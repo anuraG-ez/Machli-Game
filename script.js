168254f034251dc088a2b282b06b4564cad3c7d1
anuraG-ez <160966588+anuraG-ez@users.noreply.github.com>
2024-02-24 10:00:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let startBtn = document.querySelector(".start-btn");
+let nextBtn = document.querySelector(".next-btn");
+
+let para = document.querySelectorAll("p");
+
+
+let count = 0;
+
+startBtn.addEventListener("click",()=>{
+    count = 1;
+    para[0].innerText = `1 Machli🐠`;
+    para[1].innerText = `pani me gayi🌊 - x (1)`;
+    para[2].innerText = `chapak💦 - x (1)`;
+})
+nextBtn.addEventListener("click",()=>{
+    if(count<=0){
+        alert("First start the game!😡");
+        alert("I started it for you 🫡 :)")
+    }
+    count++;
+    
+    game(count);
+})
+
+const game = (count)=>{
+     para[0].innerText = `${count} Machli🐠`;
+     para[1].innerText = `pani me gayi🌊 - x (${count})`;
+     para[2].innerText = `chapak💦 - x (${count})`;
+}
